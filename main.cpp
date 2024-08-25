8998a49796baffdca949d6743807c20237ebcee7
Oliver Ellis <ellis.oliver@gmail.com>
2024-08-25 17:01:09 +0200

    Added create instance

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #define GLF_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
 
 #include <iostream>
 #include <stdexcept>
@@ -12,6 +13,7 @@ class HelloTriangleApplication
 {
 private: // Fields
     GLFWwindow *window;
+    VkInstance instance;
 
 public:
     void Run()
@@ -23,6 +25,35 @@ public:
     }
 
 private:
+    void CreateInstance()
+    {
+        VkApplicationInfo appInfo {};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Vulkan App";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo {};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char **glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+        {
+            throw std::runtime_error("[+] Failed to create instance.");
+        }
+    }
+
     void InitWindow()
     {
         glfwInit();
@@ -39,7 +70,7 @@ private:
 
     void InitVulkan()
     {
-
+        CreateInstance();
     }
 
     void MainLoop()
